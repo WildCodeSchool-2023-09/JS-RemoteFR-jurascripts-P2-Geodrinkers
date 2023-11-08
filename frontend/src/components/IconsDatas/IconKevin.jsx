d6759a099de020a34740aa4963e5a77d86987e19
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-11-09 00:00:54 +0100

    import pin_map on the earth and create one card for JL_pin

---
+++ frontend/src/components/IconsDatas/IconKevin.jsx
@@ -0,0 +1,54 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import PropTypes from "prop-types";
+
+export default function IconKevin({ position }) {
+  const iconRef = useRef();
+  const [cardCocktail, setCardCocktail] = useState(false);
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load("./src/assets/gltf/kevin.glb", (gltf) => {
+      const icon = gltf.scene;
+
+      icon.rotation.y = Math.PI / 2;
+
+      if (iconRef.current) {
+        iconRef.current.add(icon);
+      }
+    });
+  });
+
+  useFrame(() => {
+    if (iconRef) {
+      iconRef.current.rotation.y += 0.01;
+    }
+  });
+
+  const handleClick = () => {
+    setCardCocktail(!cardCocktail);
+  };
+
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "auto";
+  };
+
+  return (
+    <group scale={2.8} dispose={null} position={position}>
+      <mesh
+        ref={iconRef}
+        onClick={handleClick}
+        onPointerOver={handlePointerOver}
+        onPointerOut={handlePointerOut}
+      />
+    </group>
+  );
+}
+IconKevin.propTypes = {
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
