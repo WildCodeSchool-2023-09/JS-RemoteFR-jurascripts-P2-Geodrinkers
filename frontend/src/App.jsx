4b7cde62f0d11e332bb75e1856c4a103e841452a
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-11-08 15:52:33 +0100

    fix unknown import for threeJS props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
 import Home from "./pages/Home";
 import "./components/styles/Home.scss";
+import SpaceMap from "./pages/SpaceMap";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <SpaceMap />
+      <Home />
+    </>
+  );
 }
---
+++ frontend/src/components/Earth.jsx
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Preload, Stars } from "@react-three/drei";
+import earthTexture from "../assets/img/earth.jpg";
+
+function Earth() {
+  const texture = useLoader(THREE.TextureLoader, earthTexture);
+
+  return (
+    <group>
+      <mesh>
+        <sphereGeometry args={[15, 64, 64]} />
+        <meshStandardMaterial map={texture} />
+      </mesh>
+    </group>
+  );
+}
+
+export default function EarthCanvas() {
+  return (
+    <div className="space-ctn">
+      <Canvas camera={{ fov: 60, near: 0.1, far: 500, position: [0, 20, 65] }}>
+        <OrbitControls />
+        <Stars />
+        <Earth />
+        <ambientLight intensity={1.5} />
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+}
